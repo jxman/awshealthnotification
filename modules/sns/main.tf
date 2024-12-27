c8b4634e9e583b511fb0248f8bc7a61982b54b0d
John Xanthopoulos <jxman@hotmail.com>
2024-12-27 11:53:36 -0500

    Fixed lifecycle code block in sns module

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -13,6 +13,7 @@ resource "aws_sns_topic_subscription" "email_subscriptions" {
 
   lifecycle {
     create_before_destroy = true
+    replace_triggered_by  = [aws_sns_topic.health_events.arn]
   }
 }
 
@@ -25,6 +26,7 @@ resource "aws_sns_topic_subscription" "sms_subscriptions" {
 
   lifecycle {
     create_before_destroy = true
+    replace_triggered_by  = [aws_sns_topic.health_events.arn]
   }
 }
 
