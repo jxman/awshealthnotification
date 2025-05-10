878d4a7afdd789e00e7926c2d08d49c0e6d0851e
John Xanthopoulos <jxman@hotmail.com>
2025-05-10 17:12:17 -0400

    refactor: remove subscription management from Terraform
    - Remove email and SMS subscription resources
    - Subscriptions will be managed manually via AWS Console
    - Simplify module structure and variables
    - Update GitHub Actions workflow
    - Add migration script for safe transition

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,24 +1,13 @@
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "environment" {
   description = "Environment name"
   type        = string
   default     = "dev"
 }
 
-variable "email_addresses" {
-  description = "List of email addresses for notifications"
-  type        = list(string)
-}
-
-variable "phone_numbers" {
-  description = "List of phone numbers for SMS notifications"
-  type        = list(string)
-  default     = []
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "terraform_state_bucket" {
@@ -37,11 +26,10 @@ variable "terraform_state_dynamodb_table" {
 }
 
 variable "tags" {
-  description = "Resource tags"
+  description = "Default tags for all resources"
   type        = map(string)
   default = {
     Environment = "dev"
     Service     = "aws-health-notifications"
-    ManagedBy   = "terraform"
   }
 }
--- modules/sns/variables.tf
+++ modules/sns/variables.tf
@@ -3,18 +3,6 @@ variable "environment" {
   type        = string
 }
 
-variable "email_addresses" {
-  description = "List of email addresses for notifications"
-  type        = list(string)
-  default     = []
-}
-
-variable "phone_numbers" {
-  description = "List of phone numbers for SMS notifications (E.164 format)"
-  type        = list(string)
-  default     = []
-}
-
 variable "tags" {
   description = "Resource tags"
   type        = map(string)
