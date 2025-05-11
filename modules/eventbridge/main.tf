4b8595984d47a667994bbb3a573861ac1048522b
John Xanthopoulos <jxman@hotmail.com>
2025-05-11 18:39:05 -0400

    fix: use minimal EventBridge config with no input transformer

--- modules/eventbridge/main.tf
+++ modules/eventbridge/main.tf
@@ -12,19 +12,6 @@ resource "aws_cloudwatch_event_target" "sns" {
   rule      = aws_cloudwatch_event_rule.health_events.name
   target_id = "HealthNotificationTarget"
   arn       = var.sns_topic_arn
-
-  # Very basic input transformer that EventBridge accepts
-  input_transformer {
-    input_paths = {
-      service     = "$.detail.service"
-      status      = "$.detail.statusCode"
-      eventType   = "$.detail.eventTypeCode"
-      description = "$.detail.eventDescription[0].latestDescription"
-      region      = "$.region"
-      time        = "$.time"
-    }
-
-    # Simple template that works reliably with EventBridge
-    input_template = "${upper(var.environment)} Health Alert: <service> (<status>)\n\n<description>\n\nRegion: <region>\nTime: <time>"
-  }
+  
+  # No input transformer - send the raw event data
 }
